8f7d9b63e37e7f91b117f55ce9bb3e024dc042df
Rudy <bourez.rudy@gmail.com>
2022-06-20 14:54:58 +0200

    create_app() removed for azure

--- app/__init__.py
+++ app/__init__.py
@@ -3,32 +3,29 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 # init SQLAlchemy so we can use it later in our models
-db = SQLAlchemy()
+db = SQLAlchemy()    
+app = Flask(__name__)
 
-def create_app():
-    app = Flask(__name__)
+app.config['SECRET_KEY'] = 'my-secret-secret-key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 
-    app.config['SECRET_KEY'] = 'my-secret-secret-key'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-
-    db.init_app(app)
+db.init_app(app)
     
-    login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
-    login_manager.init_app(app)
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
 
-    from .models import User
+from .models import User
 
-    @login_manager.user_loader
-    def load_user(user_id):
-        # since the user_id is just the primary key of our user table, use it in the query for the user
-        return User.query.get(int(user_id))
-    # blueprint for auth routes in our app
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint)
+@login_manager.user_loader
+def load_user(user_id):
+    # since the user_id is just the primary key of our user table, use it in the query for the user
+    return User.query.get(int(user_id))
 
-    # blueprint for non-auth parts of app
-    from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
+# blueprint for auth routes in our app
+from .auth import auth as auth_blueprint
+app.register_blueprint(auth_blueprint)
 
-    return app
+# blueprint for non-auth parts of app
+from .main import main as main_blueprint
+app.register_blueprint(main_blueprint)
