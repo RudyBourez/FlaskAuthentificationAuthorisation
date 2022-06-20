200c556fec066e165b1bd6c854b5aeb4816ed3a0
Rudy <bourez.rudy@gmail.com>
2022-06-20 16:42:33 +0200

    env

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-
+from dotenv import load_dotenv
+import os
 # init SQLAlchemy so we can use it later in our models
+
+load_dotenv()
 db = SQLAlchemy()    
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'my-secret-secret-key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 
 db.init_app(app)
     
@@ -21,7 +24,6 @@ from .models import User
 def load_user(user_id):
     # since the user_id is just the primary key of our user table, use it in the query for the user
     return User.query.get(int(user_id))
-
 # blueprint for auth routes in our app
 from .auth import auth as auth_blueprint
 app.register_blueprint(auth_blueprint)
