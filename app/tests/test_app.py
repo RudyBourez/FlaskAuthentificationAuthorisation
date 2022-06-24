22b0396b822a8dfadbe572abf35d8c33a36ee323
Rudy <bourez.rudy@gmail.com>
2022-06-24 16:51:37 +0200

    pytest cov

---
+++ app/tests/test_app.py
@@ -0,0 +1,8 @@
+# import pytest
+# from app import app
+# import os
+
+# def test_app():
+#     assert app.config["SECRET_KEY"] == os.getenv('SECRET_KEY')
+#     assert app.config["SQLALCHEMY_DATABASE_URI"] == os.getenv('SQLALCHEMY_DATABASE_URI')
+#     assert app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] == os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
---
+++ app/tests/test_models.py
@@ -0,0 +1,26 @@
+from app import *
+from app.models import User, Role, UserRoles
+import pytest
+
+@pytest.fixture
+def my_user1():
+    my_user1 = User(name="Rudy", email="rudy@gmail.com", password="123456")
+    return my_user1
+
+@pytest.fixture
+def my_user2():
+    my_user2 = User(name="user", email="user@gmail.com", password="123456")
+    return my_user2
+
+@pytest.fixture
+def my_admin():
+    my_admin = Role(name="Admin")
+    return my_admin
+
+@pytest.fixture
+def my_agent():
+    my_agent = Role(name="Agent")
+    return my_agent
+
+def test_models(my_user1):
+    assert my_user1.name == "Rudy"
