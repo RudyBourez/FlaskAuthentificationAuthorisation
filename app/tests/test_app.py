c8b41f340501277e4923c93754d9e5b3a22edc62
Rudy <bourez.rudy@gmail.com>
2022-07-01 14:44:28 +0200

    selenium

--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -1,21 +1,34 @@
 import pytest
-
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 from app import app
-
 
 @pytest.fixture
 def client():
     with app.test_client() as client:
-        yield client
-        
+        yield client   
 
-def test_index(client):
-	response = client.get('/')
-	assert response.status_code == 200
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    return driver
+
+def test_index(client, driver):
+    driver.get('http://tutoflaskauth.azurewebsites.net/')
+    response = client.get('/')
+    assert driver.title == "Flask auth app"
+    assert response.status_code == 200
+    driver.close()
  
-def test_signup(client):
-	response = client.get('/signup')
-	assert response.status_code == 200
+def test_signup(client, driver):
+    driver.get('http://tutoflaskauth.azurewebsites.net/signup')
+    response = client.get('/signup')
+    h3_title = driver.find_element(By.CLASS_NAME, "title")
+    assert h3_title.text == "Sign Up"
+    assert response.status_code == 200
+    driver.close()
 
 def test_profile_without_login(client):
 	response = client.get('/profile')
